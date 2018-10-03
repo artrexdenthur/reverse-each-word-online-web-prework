c36776632b82b477d0ed0e84e204f639683d7960
Paul Ashour <paul.ashour@gmail.com>
2018-10-03 05:11:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 def reverse_each_word(sentence)
-  word_arr = sentence.split(" ")
+  word_arr = sentence.split(' ')
   word_arr.each do |word|
-    word = word.
+    word = word.reverse!
+  end
+  word_arr.join(' ')
+end
